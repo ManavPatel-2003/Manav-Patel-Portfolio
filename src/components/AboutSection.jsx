c8c5637804f03233e8701d2bdfee36105440dd4e
Manav <manavpatel92003@gmail.com>
2025-06-03 01:16:08 +0530

    About section changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User, GraduationCap, School, School2Icon, School2, LucideSchool } from "lucide-react";
+import { Briefcase, Code, User, GraduationCap, School, ExternalLink } from "lucide-react";
 
 export const AboutSection = () => {
   return (
@@ -34,10 +34,20 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1M3b_NgxRF5rtXw3EEuCL8grBPiPZhL9h/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                target="_blank"
+                download
               >
+                <div className="flex justify-between gap-2">
+                <div>
                 Resume
+                </div>
+                <div>
+                <ExternalLink size={20} />
+                </div>
+                </div>
+                
               </a>
             </div>
           </div>
